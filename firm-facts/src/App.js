2aaab1708d85b048dd43458fc1dc628a704281a1
Wendy <w.yovanska@gmail.com>
2024-09-24 11:08:36 -0600

    playing with media queries

--- firm-facts/src/App.js
+++ firm-facts/src/App.js
@@ -4,17 +4,9 @@ import FirmFacts from './components/FirmFacts';
 function App() {
   return (
     <div className="App">
-      {/* {elements} */}
-      {/* <FirmFacts 
-        text={buttons[0].text} 
-        hasIcon={buttons[0].hasIcon} 
-        status={buttons[0].status} 
-        isBgChanging={buttons[0].isBgChanging}
-        cardStatus={buttons[0].cardStatus}
-        isCentered={buttons[0].isCentered}
-      /> */}
+      <h1>Firm Facts</h1>
+      <hr />
       <FirmFacts />
-      
     </div>
   );
 }
